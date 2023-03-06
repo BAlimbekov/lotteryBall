b858bee81a68fa126efd4480ead7805c0c593854
BAlimbekov <alimbekovbekmambet@gmail.com>
2023-03-06 17:10:01 -0600

    created Ball component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Ball from "./Ball";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Ball num ={17} />
+      <Ball num ={5} />
+      <Ball num ={7} />
+      <Ball num ={40} />
+      <Ball num ={1} />
+      <Ball num ={9} />
     </div>
   );
 }
