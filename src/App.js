a3e404566d778d198468262883371a1a2e39c980
BAlimbekov <alimbekovbekmambet@gmail.com>
2023-03-06 19:12:54 -0600

    implemented dark mode in the div

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import Ball from "./Ball";
+import Lottery from './Lottery';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function toggleTheme() {
+    setIsDarkMode(!isDarkMode);
+  }
+
   return (
-    <div className="App">
-      <Ball num ={17} />
-      <Ball num ={5} />
-      <Ball num ={7} />
-      <Ball num ={40} />
-      <Ball num ={1} />
-      <Ball num ={9} />
+    <div className={isDarkMode ? "dark-mode" : "light-mode"}>
+      <div >
+        <Lottery title="Lotto" maxNum={40} numBalls={6} />
+        <Lottery title="Mini Daily" maxNum={10} numBalls={4} />
+      </div>
+      <div>
+          <button onClick={toggleTheme}>
+            {isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+          </button>
+      </div>
     </div>
   );
 }
